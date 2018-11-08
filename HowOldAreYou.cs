b688029afe6e66ba14b28f1d48e6e8901a3738c0
Nikole Tiffany Powell <TheCanterlonian@users.noreply.github.com>
2018-11-08 15:35:51 -0500

    Update HowOldAreYou.cs

--- HowOldAreYou.cs
+++ HowOldAreYou.cs
@@ -15,8 +15,15 @@ namespace HowOldAreYou
             Console.WriteLine("When were you born?");
             DateTime birthDate = DateTime.Parse(Console.ReadLine());
             TimeSpan elapsedTime = DateTime.Now.Subtract(birthDate);
+            double elapsedYears = elapsedTime.TotalDays / 365.2425;
+            double elapsedYearsRemainder = elapsedTime.TotalDays % 365.2425;
+            elapsedYears = (int)elapsedYears;
+            double elapsedYearsRemainderDaysRemainder = (int)elapsedYearsRemainder % 30;
+            elapsedYearsRemainder = (int)elapsedYearsRemainder/ 30;
             Console.WriteLine();
-            Console.WriteLine("You are " + elapsedTime.Days + " days old.");
+            Console.WriteLine("You are " + elapsedTime.Days + " days, " + elapsedTime.Hours + " hours, and " + elapsedTime.Minutes + " minutes old.");
+            Console.WriteLine();
+            Console.WriteLine("You are " + elapsedYears + " years, " + elapsedYearsRemainder + " Stellaris months, and " + elapsedYearsRemainderDaysRemainder + " days old.");
             Console.WriteLine();
             Console.WriteLine("Press any key to exit...");
             Console.ReadKey(true);
@@ -24,4 +31,3 @@ namespace HowOldAreYou
         }
     }
 }
-
